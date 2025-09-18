81f93899d71f7a8277ac7f94b2b37ff68842034a
juantome49 <juantome49@gmail.com>
2025-09-17 23:19:43 -0300

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.getElementById('login-form');
     const errorMessage = document.getElementById('error-message');
     // Asegúrate de que esta URL coincida con la de tu backend desplegado
-    const API_URL = 'https://e-commerce-qflc.onrender.com/api/login';
+    const API_URL = 'https://ecommerce-backend-byu5.onrender.com';
 
     if (loginForm) {
         loginForm.addEventListener('submit', async (e) => {
